00cbb80263831ab8d4fdb81865b9b3411c2c015b
James <csorakris@gmail.com>
2016-10-14 17:59:42 +0700

    with comment

--- srget.py
+++ srget.py
@@ -1,81 +1,99 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 
 import socket as skt 
 import sys
 from urlparse import urlparse
 
+
+#make http request
 def make_http_request(host, obj):
     NL="\r\n"
     return ("GET {o} HTTP/1.1" + NL + "Host: {s}" + NL + NL).format(o=obj, s=host)
+
+
+#extract header from the first .recv
 def header_cutter(s):
 	f=s.split("\r\n\r\n")
 	return f[0]
-def contentlength(s):
+
+
+#extract content length from header
+def content_length(s):
 	f=s.split("\r\n")
 	for e in f:
 		if "Content-Length" in e:
 			return e.split(":")
 
 
-url = sys.argv[-1]
-urlp= urlparse(url)
-srv_name = urlp.netloc
-srv_path = urlp.path
-srv_port = 80
-srv_ip = skt.gethostbyname(srv_name) #"21.224.123.42:231"
-
-client_socket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
-client_socket.connect((srv_name, srv_port))
-print "connencting"
-
-request_str = make_http_request(srv_name, srv_path)
-client_socket.send(request_str)
-print "request sent"
-
-f = open(sys.argv[2], 'wb')
-header=""
-byteCount=0
-if sys.argv[1] == "-o":
-	print "pass"
-	#find\r\n\r\n
+#get header 
+def get_header(client_socket):
+	header=""
 	while True:
 		data_received = client_socket.recv(8192)
 		header+=data_received
 		if "\r\n\r\n" in data_received:
 			break
-			client_socket.close()
-	h = header_cutter(header)
-	contentlength = int(contentlength(header)[1])
-	content = header.split("\r\n\r\n")[1]
-	byteCount+=len(content)
-	f.write(content)
-	print contentlength
-	print "SECOND"
+			
+	return header
 
-	while byteCount<=contentlength:
+
+#load and write data
+def load_write_data(byte_count, contentlength, client_socket, f):
+	while byte_count<=contentlength:
 		data_received = client_socket.recv(8192)
-		print "hello world!"
 		f.write(data_received)
-		if len(data_received)+byteCount==contentlength:
-			byteCount+=len(data_received)
-			print len(data_received)+byteCount
+		if len(data_received)+byte_count==contentlength:
+			byte_count+=len(data_received)
 			client_socket.close()
 			break
-		byteCount+=len(data_received)
-		print byteCount
-		# print "yo"
-		# print "{!r}".format(data_received), #, prevent from making a new line
+		byte_count+=len(data_received)
+	return byte_count
+
+
+def main():
+	url = sys.argv[-1]
+	urlp= urlparse(url)
+
+    #srv name and path
+	srv_name = urlp.hostname
+	srv_path = urlp.path
+	if ":" in url:
+		srv_port = urlp.port 
+	else:
+		srv_port = 80
 	
+	#create socket and connection
+	client_socket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
+	client_socket.connect((srv_name, srv_port))
 
-print byteCount==contentlength
-print byteCount
-print contentlength
-print "DONE"
+	#make and send
+	request_str = make_http_request(srv_name, srv_path)
+	client_socket.send(request_str)
+	
+	#write and load file from http
+	with open(sys.argv[2], 'wb') as f:
+		byte_count=0
+		## TO FIX: use argparse or getopt
+		if sys.argv[1] == "-o":
+			#get header
+			header= get_header(client_socket)
+			h = header_cutter(header)
+			content = header.split("\r\n\r\n")[1]
+
+			#for chunk
+			if ("Transfer-Encoding: chunked" in h) or ("chunked" in h) or ("Chunked" in h):
+				print "I die here"
+
+			#if content length exsist in header
+			elif "Content-Length" in h:
+				contentlength = int(content_length(h)[1])
+				byte_count+=len(content)
+				#write the remaining content
+				f.write(content)
+
+				#load and write data
+				byte_count=load_write_data(byte_count, contentlength, client_socket, f)
 
 
-#if connection break 
-#if there is no more data
-#did ech round data is sent 1024
-#input thong mee http paow ans mee
-#load 32 bit
-
+if __name__ == '__main__':
+	main()
